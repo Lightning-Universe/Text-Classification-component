a6d5b8062849a95045a2b0b597b084341c1e7c57
awaelchli <aedu.waelchli@gmail.com>
2022-11-23 16:41:21 +0100

    sample prediction

--- app.py
+++ app.py
@@ -5,10 +5,7 @@ from transformers import BloomTokenizerFast, BloomForSequenceClassification
 
 from lai_textclf.clf import TLDR
 
-sample_text = """
-ML Ops platforms come in many flavors from platforms that train models to platforms that label data and auto-retrain models. To build an ML Ops platform requires dozens of engineers, multiple years and 10+ million in funding. The majority of that work will go into infrastructure, multi-cloud, user management, consumption models, billing, and much more.
-Build your platform with Lightning and launch in weeks not months. Focus on the workflow you want to enable (label data then train models), Lightning will handle all the infrastructure, billing, user management, and the other operational headaches.
-"""
+sample_text = "Blue is the most beautiful color!"
 
 
 class GiveMeAName(TLDR):
@@ -48,12 +45,10 @@ class GiveMeAName(TLDR):
 
     def run(self):
         super().run()
-
-        # # Make a prediction at the end of fine-tuning
-        # if self._trainer.global_rank == 0:
-        #     predictions = predict(self._pl_module.to("cuda"), sample_text)
-        #     print("Input text:\n", sample_text)
-        #     print("Summarized text:\n", predictions[0])
+        if self._trainer.global_rank == 0:
+            prediction = predict(self._pl_module, sample_text)
+            print("Input text:\n", sample_text)
+            print("Predicted label:\n", prediction)
 
 
 app = L.LightningApp(
--- lai_textclf/lightning_module.py
+++ lai_textclf/lightning_module.py
@@ -89,62 +89,10 @@ class TextClassification(LightningModule):
         return self(**batch)[1].argmax(-1) + 1
 
 
-#
-#
-# def predict(
-#     module: LightningModule,
-#     source_text: str,
-#     max_length: int = 512,
-#     num_return_sequences: int = 1,
-#     num_beams: int = 2,
-#     top_k: int = 50,
-#     top_p: float = 0.95,
-#     do_sample: bool = True,
-#     repetition_penalty: float = 2.5,
-#     length_penalty: float = 1.0,
-#     early_stopping: bool = True,
-#     skip_special_tokens: bool = True,
-#     clean_up_tokenization_spaces: bool = True,
-# ):
-#     """
-#     generates prediction for T5/MT5 model
-#     Args:
-#         source_text (str): any text for generating predictions
-#         max_length (int, optional): max token length of prediction. Defaults to 512.
-#         num_return_sequences (int, optional): number of predictions to be returned. Defaults to 1.
-#         num_beams (int, optional): number of beams. Defaults to 2.
-#         top_k (int, optional): Defaults to 50.
-#         top_p (float, optional): Defaults to 0.95.
-#         do_sample (bool, optional): Defaults to True.
-#         repetition_penalty (float, optional): Defaults to 2.5.
-#         length_penalty (float, optional): Defaults to 1.0.
-#         early_stopping (bool, optional): Defaults to True.
-#         skip_special_tokens (bool, optional): Defaults to True.
-#         clean_up_tokenization_spaces (bool, optional): Defaults to True.
-#     Returns:
-#         list[str]: returns predictions
-#     """
-#     input_ids = module.tokenizer.encode(
-#         source_text, return_tensors="pt", add_special_tokens=True
-#     )
-#     input_ids = input_ids.to(module.device)
-#     generated_ids = module.model.generate(
-#         input_ids=input_ids,
-#         num_beams=num_beams,
-#         max_length=max_length,
-#         repetition_penalty=repetition_penalty,
-#         length_penalty=length_penalty,
-#         early_stopping=early_stopping,
-#         top_p=top_p,
-#         top_k=top_k,
-#         num_return_sequences=num_return_sequences,
-#     )
-#     preds = [
-#         module.tokenizer.decode(
-#             g,
-#             skip_special_tokens=skip_special_tokens,
-#             clean_up_tokenization_spaces=clean_up_tokenization_spaces,
-#         )
-#         for g in generated_ids
-#     ]
-#     return preds
+def predict(module: LightningModule, source_text: str):
+    inputs = module.tokenizer(source_text, return_tensors="pt")
+    with torch.no_grad():
+        _, logits = module(**inputs)
+    predicted_class_id = logits.argmax().item()
+    label = module.model.config.id2label[predicted_class_id]
+    return label
