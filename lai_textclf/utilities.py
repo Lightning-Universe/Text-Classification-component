add9309d985682ea25ffa7e3e4d58d1d2a5f2dd0
Justus Schock <justus.schock@posteo.de>
2022-12-09 18:10:45 +0100

    update reviewer comments

--- lai_textclf/utilities.py
+++ lai_textclf/utilities.py
@@ -8,7 +8,7 @@ def warn_if_drive_not_empty(drive: Drive):
 
     if drive.list():
         warnings.warn(
-            "Drive is not empty! This may result into wrong logging behaviour if your app doesn't have a built-in resume mechanism. Consider deleting the .lightning file and restarting the app or giving it a new name with the --name flag of 'lightning run app'."
+            "Drive is not empty! This may result in wrong logging behaviour if your app doesn't have a built-in resume mechanism. Consider deleting the .lightning file and restarting the app or giving it a new name with the --name flag of 'lightning run app'."
         )
 
 
