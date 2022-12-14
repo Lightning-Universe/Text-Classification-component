3358ad6680645564252bb0398f4f55b283fe719e
Carlos Mocholí <carlossmocholi@gmail.com>
2022-12-14 23:11:23 +0100

    Use `curl --create-dirs` (#22)

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 #! pip install git+https://github.com/Lightning-AI/LAI-Text-Classification-Component
-#! mkdir -p ${HOME}/data/yelpreviewfull
-#! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/train.csv -o ${HOME}/data/yelpreviewfull/train.csv -C -
-#! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/test.csv -o ${HOME}/data/yelpreviewfull/test.csv -C -
+#! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/train.csv --create-dirs -o ${HOME}/data/yelpreviewfull/train.csv -C -
+#! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/test.csv --create-dirs -o ${HOME}/data/yelpreviewfull/test.csv -C -
 
 import os
 from copy import deepcopy
