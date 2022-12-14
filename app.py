dea1225e1cd2cfe49a36db257cab2b5089a514a7
Carlos Mocholí <carlossmocholi@gmail.com>
2022-12-14 22:18:33 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 #! pip install git+https://github.com/Lightning-AI/LAI-Text-Classification-Component
 #! mkdir -p ${HOME}/data/yelpreviewfull
-#! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/train.csv -o ${HOME}/data/yelpreviewfull/train.csv
-#! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/test.csv -o ${HOME}/data/yelpreviewfull/test.csv
+#! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/train.csv -o ${HOME}/data/yelpreviewfull/train.csv -C -
+#! curl https://s3.amazonaws.com/pl-flash-data/lai-llm/lai-text-classification/datasets/Yelp/datasets/YelpReviewFull/yelp_review_full_csv/test.csv -o ${HOME}/data/yelpreviewfull/test.csv -C -
 
 import os
 from copy import deepcopy
