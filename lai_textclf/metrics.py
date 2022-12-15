4bcd41319ecfdc2a125adf5f8d1395140323b802
Justus Schock <12886177+justusschock@users.noreply.github.com>
2022-12-15 18:25:50 +0100

    shortening (#24)

--- lai_textclf/metrics.py
+++ lai_textclf/metrics.py
@@ -3,7 +3,7 @@ from torchmetrics.classification import (MulticlassAccuracy, MulticlassF1Score,
                                          MulticlassPrecision, MulticlassRecall)
 
 
-def get_default_clf_metrics(num_classes: int):
+def clf_metrics(num_classes: int):
     return MetricCollection(
         MulticlassPrecision(num_classes),
         MulticlassRecall(num_classes),
